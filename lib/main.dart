ffc799f0801f2580c388176fa1526e12fca70419
Boussari Latif <latif.boussari@mehriso.com>
2019-12-05 11:53:45 +0100

    get transaction information

--- lib/main.dart
+++ lib/main.dart
@@ -4,10 +4,13 @@ import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 import 'dart:core';
 
+import 'package:webview_project/utils/Kkiapay.dart';
 
+final Kkiapay kkiapay =
+    Kkiapay(apikey: 'f1e7270098f811e99eae1f0cfc677927', sandbox: true);
 
-
-final url = 'https://widget.kkiapay.me/?=eyJhbW91bnQiOiIxIiwiY2FsbGJhY2siOiJodHRwOi8vcmVkaXJlY3Qua2tpYXBheS5tZSIsImRhdGEiOiJMZW9uZWwgemVndWUiLCJob3N0IjoiY28ub3BlbnNpLm1lZGljYWwiLCJrZXkiOiJmMWU3MjcwMDk4ZjgxMWU5OWVhZTFmMGNmYzY3NzkyNyIsIm5hbWUiOiJITlMgSWl5YW1hIiwicGhvbmUiOiI5NzAwMDAwMCIsInJlYXNvbiI6IlBhaWVtZW50IGRcdTAwMjd1biByZW5kZXotdm91cyIsInNhbmRib3giOnRydWUsInNkayI6ImFuZHJvaWQiLCJ0aGVtZSI6IiMyYmEzNTkiLCJ1cmwiOiJodHRwczovL2FwaS5ra2lhcGF5Lm1lL3V0aWxzL2ZpbGUvenNlMmtVcDZoZ2REUnBzMU9CcGtTSHhSRSJ9';
+final url =
+    'https://widget.kkiapay.me/?=eyJhbW91bnQiOiIxIiwiY2FsbGJhY2siOiJodHRwOi8vcmVkaXJlY3Qua2tpYXBheS5tZSIsImRhdGEiOiJMZW9uZWwgemVndWUiLCJob3N0IjoiY28ub3BlbnNpLm1lZGljYWwiLCJrZXkiOiJmMWU3MjcwMDk4ZjgxMWU5OWVhZTFmMGNmYzY3NzkyNyIsIm5hbWUiOiJITlMgSWl5YW1hIiwicGhvbmUiOiI5NzAwMDAwMCIsInJlYXNvbiI6IlBhaWVtZW50IGRcdTAwMjd1biByZW5kZXotdm91cyIsInNhbmRib3giOnRydWUsInNkayI6ImFuZHJvaWQiLCJ0aGVtZSI6IiMyYmEzNTkiLCJ1cmwiOiJodHRwczovL2FwaS5ra2lhcGF5Lm1lL3V0aWxzL2ZpbGUvenNlMmtVcDZoZ2REUnBzMU9CcGtTSHhSRSJ9';
 
 // void main() => runApp());
 void main() => runApp(App());
@@ -28,20 +31,21 @@ class Myapp extends StatefulWidget {
 
 class _MyappState extends State<Myapp> {
   @override
-  final Completer<WebViewController> _controller = Completer<WebViewController>();
+  final Completer<WebViewController> _controller =
+      Completer<WebViewController>();
 
-
-  String getTransactionId(String url){
+  String getTransactionId(String url) {
     final link = Uri.parse(url);
     return link.queryParameters['transaction_id'];
   }
-  
 
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('salut les gens'),),
+      appBar: AppBar(
+        title: Text('salut les gens'),
+      ),
       body: SafeArea(
-              child: WebView(
+        child: WebView(
           initialUrl: url,
           javascriptMode: JavascriptMode.unrestricted,
           onWebViewCreated: (WebViewController webview) async {
@@ -50,19 +54,25 @@ class _MyappState extends State<Myapp> {
             print('url');
             _controller.complete(webview);
           },
-          navigationDelegate: (NavigationRequest request){
+          navigationDelegate: (NavigationRequest request) {
             print('request is there');
-            print(request.url);
-            return NavigationDecision.navigate;
+            final transactionId = getTransactionId(request.url);
+            kkiapay.getTransactionInfo(transactionId).then((response) {
+              // print
+
+              if (response['status'] == 'SUCCESS') {
+                print('do stuff there');
+              }
+            }).catchError((onError) {
+              print(onError);
+              print('Internal Server Error');
+            });
           },
-          onPageFinished: (String url){
+          onPageFinished: (String url) {
             print('Page finished loading: $url');
           },
-         
-          
         ),
       ),
     );
   }
 }
-
--- lib/utils/Kkiapay.dart
+++ lib/utils/Kkiapay.dart
@@ -1,15 +1,34 @@
 import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+import 'package:meta/meta.dart';
 
 class Kkiapay {
-
   String apikey;
   bool sandbox;
+  //
+  String amount;
+  String data;
+  String host;
+  String theme;
+  String phone;
+  String callback;
 
-  Kkiapay({this.apikey, this.sandbox = false});
+  //
+  final _sandboxEndpoint = 'https://api-sandbox.kkiapay.me';
+  final _liveEndpoint = 'https://api.kkiapay.me';
 
+  String get _endpoint => sandbox ? _sandboxEndpoint : _liveEndpoint;
 
-  Future getTransactionInfo(){
-    //implement function for get transaction info
+  Kkiapay(
+      {@required this.apikey,
+      this.sandbox = false,
+      this.callback = 'http://redirect.kkiapay.me'});
+
+  Future<Map> getTransactionInfo(transactionId) async {
+    final response = await http.post('$_endpoint/api/v1/transactions/status',
+        headers: {'x-api-key': apikey}, body: {'transactionId': transactionId});
+    Map responseObject = json.decode(response.body);
+    return responseObject;
   }
-  
 }
