37b87543f0f90f2381d435890dfacda47ffd3216
Boussari Latif <latif.boussari@mehriso.com>
2019-12-05 11:01:29 +0100

    show webview with sucessuful

---
+++ lib/main.dart
@@ -0,0 +1,68 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+import 'dart:core';
+
+
+
+
+final url = 'https://widget.kkiapay.me/?=eyJhbW91bnQiOiIxIiwiY2FsbGJhY2siOiJodHRwOi8vcmVkaXJlY3Qua2tpYXBheS5tZSIsImRhdGEiOiJMZW9uZWwgemVndWUiLCJob3N0IjoiY28ub3BlbnNpLm1lZGljYWwiLCJrZXkiOiJmMWU3MjcwMDk4ZjgxMWU5OWVhZTFmMGNmYzY3NzkyNyIsIm5hbWUiOiJITlMgSWl5YW1hIiwicGhvbmUiOiI5NzAwMDAwMCIsInJlYXNvbiI6IlBhaWVtZW50IGRcdTAwMjd1biByZW5kZXotdm91cyIsInNhbmRib3giOnRydWUsInNkayI6ImFuZHJvaWQiLCJ0aGVtZSI6IiMyYmEzNTkiLCJ1cmwiOiJodHRwczovL2FwaS5ra2lhcGF5Lm1lL3V0aWxzL2ZpbGUvenNlMmtVcDZoZ2REUnBzMU9CcGtTSHhSRSJ9';
+
+// void main() => runApp());
+void main() => runApp(App());
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Myapp(),
+    );
+  }
+}
+
+class Myapp extends StatefulWidget {
+  @override
+  _MyappState createState() => _MyappState();
+}
+
+class _MyappState extends State<Myapp> {
+  @override
+  final Completer<WebViewController> _controller = Completer<WebViewController>();
+
+
+  String getTransactionId(String url){
+    final link = Uri.parse(url);
+    return link.queryParameters['transaction_id'];
+  }
+  
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('salut les gens'),),
+      body: SafeArea(
+              child: WebView(
+          initialUrl: url,
+          javascriptMode: JavascriptMode.unrestricted,
+          onWebViewCreated: (WebViewController webview) async {
+            final url = await webview.currentUrl();
+            print(url);
+            print('url');
+            _controller.complete(webview);
+          },
+          navigationDelegate: (NavigationRequest request){
+            print('request is there');
+            print(request.url);
+            return NavigationDecision.navigate;
+          },
+          onPageFinished: (String url){
+            print('Page finished loading: $url');
+          },
+         
+          
+        ),
+      ),
+    );
+  }
+}
+
---
+++ lib/utils/Kkiapay.dart
@@ -0,0 +1,15 @@
+import 'package:http/http.dart' as http;
+
+class Kkiapay {
+
+  String apikey;
+  bool sandbox;
+
+  Kkiapay({this.apikey, this.sandbox = false});
+
+
+  Future getTransactionInfo(){
+    //implement function for get transaction info
+  }
+  
+}
