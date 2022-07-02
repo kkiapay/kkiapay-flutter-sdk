b12193a31318faa6ee23e9529663d4dad1b09574
ZEGUE Léonel <leonel.zegue@mehriso.com>
2022-07-02 14:45:58 +0100

    chore: clean test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -6,43 +6,42 @@
 // tree, read text, and verify that the values of widget properties are correct.
 
 // import 'package:webview_project/main.dart';
-//import 'package:kkiapay_flutter_sdk/utils/Kkiapay.dart';
+import 'package:kkiapay_flutter_sdk/kkiapayWebview.dart';
 
-/* void main() {
-  final kkiapay =
-      KKiaPay(key: 'apikeyisthere', amount: '3000', sucessCallback: null);
+void main() {
+  // final kkiapay =
+  //     KKiaPay(apikey: 'apikeyisthere', amount: '3000', callback: null);
 
-  final mapJson = {
-    "performed_at": "2019-12-05T10:03:44.274Z",
-    "type": "DEBIT",
-    "status": "FAILED",
-    "source": "MOBILE_MONEY",
-    "source_common_name": "mtn-benin",
-    "amount": 1000,
-    "fees": 0,
-    "net": 0,
-    "paymentlink": null,
-    "country": "BJ",
-    "reason": "processing_error",
-    "state": null,
-    "transactionId": "bxEu9odU",
-    "performedAt": "05/12/2019"
-  };
-  test('Test get transaction info function', () async {
-    kkiapay.client = MockClient((request) async {
-      return Response(json.encode(mapJson), 200);
-    });
+  // final mapJson = {
+  //   "performed_at": "2019-12-05T10:03:44.274Z",
+  //   "type": "DEBIT",
+  //   "status": "FAILED",
+  //   "source": "MOBILE_MONEY",
+  //   "source_common_name": "mtn-benin",
+  //   "amount": 1000,
+  //   "fees": 0,
+  //   "net": 0,
+  //   "paymentlink": null,
+  //   "country": "BJ",
+  //   "reason": "processing_error",
+  //   "state": null,
+  //   "transactionId": "bxEu9odU",
+  //   "performedAt": "05/12/2019"
+  // };
+  // test('Test get transaction info function', () async {
+  //   kkiapay.client = MockClient((request) async {
+  //     return Response(json.encode(mapJson), 200);
+  //   });
 
-    final item = await kkiapay.getTransactionInfo('bxEu9odU');
-    expect(item['transactionId'], "bxEu9odU");
+  //   final item = await kkiapay.getTransactionInfo('bxEu9odU');
+  //   expect(item['transactionId'], "bxEu9odU");
 
-    // kkiapay.getTransactionInfo(transactionId)
-  });
+  //   // kkiapay.getTransactionInfo(transactionId)
+  // });
 
-  test('check if function for convert to base64 retun String', () async {
-    final base64expected = kkiapay.convertObjectToBase64(mapJson);
+  // test('check if function for convert to base64 retun String', () async {
+  //   final base64expected = kkiapay.convertObjectToBase64(mapJson);
 
-    expect(base64expected, isA<String>());
-  });
+  //   expect(base64expected, isA<String>());
+  // });
 }
- */
