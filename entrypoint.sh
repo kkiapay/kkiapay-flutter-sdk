280a8f574a6335d7fd7b30a016986eb72cbad959
ZEGUE Léonel <leonel.zegue@mehriso.com>
2022-07-02 14:50:50 +0100

    chore: skip test

--- entrypoint.sh
+++ entrypoint.sh
@@ -171,7 +171,7 @@ check_required_inputs
 switch_working_directory
 detect_flutter_package || true
 get_local_package_version
-run_unit_tests || true
+run_unit_tests
 get_remote_package_version || true
 format || true
 publish || true
